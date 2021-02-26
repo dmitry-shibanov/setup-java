ba8fb90cad371fad4a4fa1f78099638a8e25eaf5
Dmitry Shibanov <dmitry-shibanov@github.com>
2021-02-26 11:54:49 +0300

    change normalizeVersion

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -23122,13 +23122,14 @@ class JavaBase {
         // TO-DO: rework ea/ga logic
         if (version.endsWith('-ea')) {
             // convert e.g. 14-ea to 14.0.0-ea
-            if (version.indexOf('.') == -1) {
-                version = version.slice(0, version.length - 3) + '.0.0-ea';
-            }
-            // match anything in -ea.X (semver won't do .x matching on pre-release versions)
-            if (version[0] >= '0' && version[0] <= '9') {
-                version = '>=' + version;
-            }
+            //   if (version.indexOf('.') == -1) {
+            //     version = version.slice(0, version.length - 3) + '.0.0-ea';
+            //   }
+            //   // match anything in -ea.X (semver won't do .x matching on pre-release versions)
+            //   if (version[0] >= '0' && version[0] <= '9') {
+            //     version = '>=' + version;
+            //   }
+            version.replace('-ea', '');
             stable = false;
         }
         if (!semver_1.default.validRange(version)) {
