d55f1f32ced053aec41e5649d11c67763b44653d
Maxim Lobanov <v-malob@microsoft.com>
2021-03-03 13:52:31 +0300

    fix test sh file

--- __tests__/verify-java.sh
+++ __tests__/verify-java.sh
@@ -5,20 +5,28 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-java_version="$(java -version 2>&1)"
-echo "Found java version: $java_version"
-grep_result=$(echo $java_version | grep "^openjdk version \"$1")
-if [ -z "$grep_result" ]; then
-  echo "::error::Unexpected version"
-  exit 1
-fi
-
 if [ -z "$2" ]; then
   echo "::error::Must supply java path argument"
   exit 1
 fi
 
-if [ "$2" != "$JAVA_HOME" ]; then
+EXPECTED_JAVA_VERSION=$1
+EXPECTED_PATH=$2
+
+if [[ $EXPECTED_JAVA_VERSION == 8.* ]]; then
+  $EXPECTED_JAVA_VERSION="1.${EXPECTED_JAVA_VERSION}"
+fi
+
+ACTUAL_JAVA_VERSION="$(java -version 2>&1)"
+echo "Found java version: $ACTUAL_JAVA_VERSION"
+
+GREP_RESULT=$(echo $ACTUAL_JAVA_VERSION | grep "^openjdk version \"$EXPECTED_JAVA_VERSION")
+if [ -z "$GREP_RESULT" ]; then
+  echo "::error::Unexpected version"
+  exit 1
+fi
+
+if [ "$EXPECTED_PATH" != "$JAVA_HOME" ]; then
   echo "::error::Unexpected path"
   exit 1
 fi
